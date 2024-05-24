3f3268e927f4e0ae78cf70c72300030f20db94c1
Samiksha <mailtosamiksha.s@gmail.com>
2024-05-24 10:14:50 +0000

    fixed AWS region name format

--- mangomore/settings.py
+++ mangomore/settings.py
@@ -168,7 +168,7 @@ LOGIN_REDIRECT_URL = '/profile/'
 if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'mangomore-p5'
-    AWS_S3_REGION_NAME = 'Asia Pacific (Sydney) ap-southeast-2'
+    AWS_S3_REGION_NAME = 'ap-southeast-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
