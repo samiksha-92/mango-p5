de78d4e66d7dbf7b6aca51016d44c732ac76aa92
Samiksha <mailtosamiksha.s@gmail.com>
2024-05-24 10:08:29 +0000

    Fix dependencies and settings

--- mangomore/settings.py
+++ mangomore/settings.py
@@ -43,8 +43,7 @@ INSTALLED_APPS = [
     'allauth.account',  # new
     'allauth.socialaccount',  # new
     'home',
-    'storages'
-    #'products',
+    'storages',
     'allauth.socialaccount.providers.github',  # new for GitHub provider
 
 ]
@@ -184,7 +183,7 @@ if 'USE_AWS' in os.environ:
 # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-        
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
